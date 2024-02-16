65a51ade0d68d58ca27ecb95f9f33a951141a49c
hewan <hewanabate2017@gmail.com>
2024-02-16 13:59:27 -0600

    Homework

---
+++ script.js
@@ -0,0 +1,64 @@
+//Part 1: Fizz Buzz
+//====================================================
+// Loop through all numbers from 1 to 100.
+/* let i = 100
+
+for(i=0; i<=100; i++)
+console.log(i); */
+// If a number is divisible by 3, log “Fizz.”
+
+/* for(i=0; i<=100; i++){
+    if(i % 3 === 0){
+
+        console.log(i, "Fizz");
+        }
+    } */
+
+ // If a number is divisible by 5, log “Buzz.”   
+/* for(i=0; i<=100; i++){
+    if(i % 5 === 0){
+
+        console.log(i, "Buzz");
+        }
+    } */
+
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+    /* for(i=0; i<=100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i, "Fizz Buzz");
+        }
+    }
+ */
+    // If a number is not divisible by either 3 or 5, log the number.
+
+    /* for(i=0; i<=100; i++){
+        if(i % 3 !== 0 && i % 5 !== 0){
+            console.log(i);
+            }
+        } */
+    
+// Part 2: Prime Time
+//====================================================
+
+/* let isPrime = true
+let n=11;
+        for(let i = 2;  i<n; i++) {
+            if(n % i === 0) {
+                isPrime = false;
+                console.log(`${n} is not a prime number`);
+            }
+        }
+        
+        if(isPrime) {
+             console.log(`${n} is a prime number.`)
+        }  */
+ 
+// Part 3: Feeling Loopy
+
+/* const cell1= Index;
+const cell2= Mass (kg);
+const cell3 = Spring 1 (m)
+const cell4 = Spring 2 (m)
+\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232 */
+
